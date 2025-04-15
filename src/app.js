14d0c12ef62c4239b240dc33e085d073775fcf03
Sasidhoni26 <sasidhoni2626@gmail.com>
2025-04-15 07:31:37 +0530

    testlog

--- src/app.js
+++ src/app.js
@@ -6,8 +6,5 @@ app.get('/api/test', (req, res) => {
     res.json({ message: 'TNPSC Quiz Backend Working!' });
 });
 
-// Start the server
-const PORT = process.env.PORT || 5656;
-app.listen(PORT, (req, res) => {
-    console.log(`Server running on port ${PORT}`);
-});
+// Export the app for Vercel
+module.exports = app;
